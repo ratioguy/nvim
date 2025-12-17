46a8e06888189b7e29eac41e6eebd05f48c90c28
ratioguy <176092341+ratioguy@users.noreply.github.com>
2025-12-16 18:18:30 -0800

    fix error with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "python","bash","c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
@@ -9,7 +9,6 @@ require'nvim-treesitter.configs'.setup {
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
     -- the name of the parser)
     -- list of language that will be disabled
-    disable = { "c", "rust" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
