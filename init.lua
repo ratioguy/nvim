775260bdc247042afed4addde5ac9f4188ff7036
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-01-10 16:35:20 -0800

    treesitter skill issue

--- init.lua
+++ init.lua
@@ -16,8 +16,6 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
 Plug('ellisonleao/gruvbox.nvim')
-Plug('nvim-treesitter/nvim-treesitter')
-Plug('https://github.com/neovim/nvim-lspconfig')
 Plug('lukas-reineke/indent-blankline.nvim')
 Plug('lewis6991/gitsigns.nvim')
 Plug('MeanderingProgrammer/render-markdown.nvim')
@@ -28,6 +26,5 @@ vim.call('plug#end')
 -- Require plugin configs
 require('config.mappings')
 require('config.options')
-require('plugins.treesitter')
 require('plugins.indent-blankline')
 require('plugins.gitsigns')
