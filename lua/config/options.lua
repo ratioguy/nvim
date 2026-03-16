cd687f3f4aa4b24a29465264af26b5f35dcc6f0d
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-03-15 18:42:10 -0700

    update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,9 @@
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
+-- Disable swapfile
+vim.o.swapfile = false 
+
 -- Make mouse copy from terminal
 vim.o.mouse = "c"
 
@@ -26,5 +29,5 @@ vim.cmd('set relativenumber')
 vim.cmd('au InsertEnter,CmdLineEnter * set norelativenumber | redraw')
 vim.cmd('au InsertLeave,CmdlineLeave * set relativenumber')
 
--- Make backspace backspace
+-- Make the backspace key backspace
 vim.opt.backspace = {'indent', 'eol', 'start'}
