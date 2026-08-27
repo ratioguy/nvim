e3f1c026d8165da3f0f1597b82be082f5ebca772
ratioguy <176092341+ratioguy@users.noreply.github.com>
2026-08-26 17:06:12 -0700

    delete arrow binds & fix comment

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,11 +1,5 @@
 -- My bindings
 local map = vim.keymap.set
 
--- Ctrl + hjkl to move arrows in insert mode
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
-
 -- Bind nerdtree to Ctrl + p
-map("n", "<C-p>", "<cmd>FzfLua files<CR>", { desc = "Toggle nerdtree window" })
+map("n", "<C-p>", "<cmd>FzfLua files<CR>", { desc = "Toggle fzf window" })
